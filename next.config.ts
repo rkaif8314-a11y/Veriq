a8d4264f48e7e9ffe82639ee02a121f51aa141b7
Kaif Ali <rkaif8314@gmail.com>
2026-09-21 09:02:52 +0530

    security: hide Next.js powered-by response header

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const securityHeaders = [
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
   async headers() {
     return [{ source: "/(.*)", headers: securityHeaders }];
   },
